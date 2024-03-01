0d9fb33aba2335eecf56d52f0e56e8e582af2172
Michael Healy <mhealy.dev@gmail.com>
2024-02-29 20:17:07 -0800

    fix: update app to include new formatting/features

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-let formatted = "";
+$(document).ready(function () {
+  let formatted = "";
 
-$("#inp").bind("click", function () {
-  let data = $("#data").val().trim();
-  let rows = data.split("\n");
-  formatted = "";
-
-  if (rows.length > 1) {
-    rows.forEach(function (r) {
-      formatted += `'${r.trim()}',\n`;
-    });
-
-    $("#data").val(formatted.slice(0, -2));
-    $("#copy").prop("disabled", false);
-  } else {
-    alert("Enter more values!");
+  function toggleButtons() {
+    const dataLength = $("#data").val().trim().length;
+    $("#copy").prop("disabled", dataLength === 0);
+    $("#clear").prop("disabled", dataLength === 0);
   }
-});
 
-$("#copy").bind("click", function () {
-  navigator.clipboard.writeText(formatted.slice(0, -2));
-  $("#data").val("");
-  $("#copy").prop("disabled", true);
+  $("#inp").click(function () {
+    let delimiter = $("#delimiter").val().trim();
+    if (!delimiter) {
+      delimiter = "'";
+    }
+    let isQuote = delimiter === "'" || delimiter === '"';
+    let data = $("#data").val().trim();
+    let rows = data.split("\n");
+
+    if (isQuote) {
+      formatted = rows
+        .map((row) => `${delimiter}${row.trim()}${delimiter},`)
+        .join("\n")
+        .slice(0, -1);
+    } else {
+      formatted =
+        rows.map((row) => row.trim()).join(` ${delimiter} \n`) +
+        (rows.length > 1 ? ` ${delimiter}` : "");
+    }
+
+    if (rows.length > 1 || rows[0] !== "") {
+      $("#data").val(formatted);
+      toggleButtons();
+    } else {
+      alert("Please enter some text!");
+    }
+  });
+
+  $("#copy").click(function () {
+    navigator.clipboard.writeText($("#data").val()).then(() => {
+      alert("Text copied to clipboard!");
+    });
+  });
+
+  // Clear button functionality
+  $("#clear").click(function () {
+    $("#data").val("");
+    toggleButtons();
+  });
+
+  $("#data").keyup(toggleButtons);
+
+  toggleButtons();
 });
